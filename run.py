dc5dc17b25c73abd42fc580d3713010762e0bb92
Sam <95234087+SamMartin92@users.noreply.github.com>
2025-05-02 07:52:52 +0000

    add wrapping logic, not tested

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import time
+
 class Grid:
     def __init__(self, cols, rows):
         self.cols = cols
@@ -20,44 +22,45 @@ class Grid:
     def count_living_neighbours(self,row,col):
         count=0
         for coord in self.neighbour_coords:
-            neighbour_coord= (row+coord[0],col+coord[1])
-            if neighbour_coord[0] in [-1, len(self.grid)]:
-                continue
-            elif neighbour_coord[1] in [-1, len(self.grid[0])]:
-                continue
-            else:
-                neighbour = self.grid[neighbour_coord[0]][neighbour_coord[1]]
+            neighbour_coord= (row+coord[0] if row+coord[0] != len(self.grid) else 0,
+                              col+coord[1] if col+coord[1] != len(self.grid) else 0)
+            neighbour = self.grid[neighbour_coord[0]][neighbour_coord[1]]
             if neighbour.alive:
                 count+=1
-        
         return count
 
 
     def update_cell_state(self,row,col):
         cell = self.grid[row][col]
-        living_neighbour_count = self.count_living_neighbours(row,col)
-        print(living_neighbour_count)
+
+        cell.neighbours = self.count_living_neighbours(row,col)
 
         if cell.alive:
-            if living_neighbour_count <2:
-                cell.alive = False # cell dies if 0 or 1 neighbours
-            elif living_neighbour_count >3:
-                cell.alive = False # cell dies if 4 or more neighbours
+            if cell.neighbours <2:
+                return False # cell dies if 0 or 1 neighbours
+            elif cell.neighbours >3:
+                return False # cell dies if 4 or more neighbours
             else:
-                pass
-                 # do nothing if 2 or 3 neighbours
+                return True
+                 #do nothing if 2 or 3 neighbours
         else:
-            if living_neighbour_count == 3:
-                cell.alive = True # cell comes to life if 3 neighbours
+            if cell.neighbours == 3:
+                return  True # cell comes to life if 3 neighbours
+        
         
 
+
+
+
     def generate_next_grid(self):
+        new_grid = Grid(self.rows,self.cols)
+
         for row in range(len(self.grid)):
             for col in range(len(self.grid[0])):
-                self.update_cell_state(row,col)
+                new_grid.grid[row][col].alive = grid.update_cell_state(row,col)
+
         
-        self.display_grid()
-        
+        return new_grid
     
 
 
@@ -66,8 +69,8 @@ class Cell:
         self.col = col
         self.row = row
         self.alive = False
+        self.neighbours = 0
         
-
 
     def __str__(self):
         return '#' if self.alive else '.'
@@ -75,17 +78,35 @@ class Cell:
 
 
 if __name__ == "__main__":  
-    grid = Grid(5,5)
+    grid = Grid(10,10)
+     
      
     grid.grid[2][2].alive = True
     grid.grid[3][2].alive = True
     grid.grid[1][2].alive = True
+    grid.grid[0][1].alive = True
+    grid.grid[1][3].alive = True
+    grid.grid[4][4].alive = True
+    grid.grid[1][3].alive = True
+    grid.grid[4][4].alive = True
+    grid.grid[1][3].alive = True
+    grid.grid[6][6].alive = True
+    grid.grid[6][7].alive = True
+    grid.grid[7][8].alive = True
+    grid.grid[6][8].alive = True
+    grid.grid[8][9].alive = True
+    grid.grid[9][8].alive = True
+    grid.grid[1][1].alive = True
+    grid.grid[1][2].alive = True
+    grid.grid[2][1].alive = True
+    grid.grid[2][2].alive = True
 
-
+    
     grid.display_grid()
+    while True:
+        time.sleep(1)
+        grid = grid.generate_next_grid()
+        grid.display_grid()
 
-    print('\n\n')
-
-    grid.generate_next_grid()
 
 
